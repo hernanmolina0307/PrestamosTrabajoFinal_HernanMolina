1673a5459bc95e128248a1cd94679645848425b6
hernanmolina0307 <molina.hernan.julio@gmail.com>
2023-01-18 21:37:12 -0300

    segundo commit

--- apis.js
+++ apis.js
@@ -1,21 +1,29 @@
-const lista = document.querySelector('#listado');
+// cotizacion del dolar api con fetch
+const cotizacion = async () => {
+    const url = "https://www.dolarsi.com/api/api.php?type=valoresprincipales";
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+    }
+    // recorrer array con for each y mostrar en html
+    cotizacion().then(data => {
+    data.forEach(element => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="card">
+    <div class="card-body">
+    <h5 class="card-title">${element.casa.nombre}</h5>
+    <p class="card-text">Compra: ${element.casa.compra}</p>
+    <p class="card-text">Venta: ${element.casa.venta}</p>
+    </div>
+    </div>
+    `;
+    document.getElementById("cotizacion").appendChild(div);
+    });
+    });
+    
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(resp => resp.json())
-    .then(data => {
-        data.forEach(post => {
-            const li = document.createElement('li')
-            li.innerHTML = `
-                <h4>${post.title}</h4>
-                <p>${post.body}</p>
-            `
-            lista.appendChild(li)
-        });
-    })
-
-
-
-
+    
 
 
 
